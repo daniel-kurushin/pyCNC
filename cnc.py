c102654b2cc1b467aab175e7d8893061869fc819
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2020-03-14 19:50:02 +0500

    test_move

--- cnc.py
+++ cnc.py
@@ -2,6 +2,10 @@ import sys
 from serial import Serial, SerialException
 
 PORTS = ['/dev/ttyUSB0', '/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3',]
+DESCR = b"Grbl 0.9j-XXY ['$' for help]\r\n"
+
+class NotACNCPortException(SerialException):
+    pass
 
 class CNC():
     def __init__(self):
@@ -10,7 +14,8 @@ class CNC():
             try:
                 print(port)
                 self.cncport = Serial(port = port, baudrate = 115200, timeout = 2)
-                print(self.cncport.readlines())
+                if DESCR not in self.cncport.readlines():
+                    raise NotACNCPortException
                 break
             except SerialException as e:
                 pass
@@ -23,8 +28,22 @@ class CNC():
             self.cncport.close()
         except AttributeError:
             print('nothing to close!', file = sys.stderr)
+            
+    def go_z(self, amount = 0.0):
+        self.cncport.write(b'G0Z%s\r\n' % bytes(str(amount), 'ascii'))
+                
+    def go_y(self, amount = 0.0):
+        self.cncport.write(b'G0Y%s\r\n' % bytes(str(amount), 'ascii'))
+                
+    def go_x(self, amount = 0.0):
+        self.cncport.write(b'G0X%s\r\n' % bytes(str(amount), 'ascii'))
                 
 if __name__ == '__main__':
     cnc = CNC()
+    cnc.go_x(10)
+    cnc.go_y(10)
+    cnc.go_z(10)
+    print(cnc.cncport.readlines())
     del cnc
+    
     
