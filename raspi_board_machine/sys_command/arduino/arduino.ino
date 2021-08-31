f9cd3bcd99e7f060b5376e0cbb122d5d9a17eaa4
Honepa <cot1884@gmail.com>
2021-08-31 22:47:08 +0500

    parse ala json

--- raspi_board_machine/sys_command/arduino/arduino.ino
+++ raspi_board_machine/sys_command/arduino/arduino.ino
@@ -2,7 +2,8 @@
 void setup() 
 {
   Serial.begin(9600); 
-
+  //Serial.setTimeout(10);
+  pinMode(13, OUTPUT);
 }
 
 float data = 0;
@@ -10,9 +11,38 @@ int state_begin, state_end;
 
 void loop() 
 {
+  
   String pars = "{'state': 'ready', 'cmd': 'wait', 'data': 0}";
+  String pars_state = "'state' : 'ready',";
+  String pars_cmd = "'cmd' : 'wait',";
+  String pars_data = "'data' : '0'";
   String state, cmd, data;
-  int j = 0;
+
+  state_begin = pars_state.indexOf("'", pars_state.indexOf(':'));
+  state_end = pars_state.lastIndexOf("'");
+  for(int i = state_begin + 1; i < state_end; i++)
+  {
+    state.concat(pars_state[i]);
+  }
+
+  state_begin = pars_cmd.indexOf("'", pars_cmd.indexOf(':'));
+  state_end = pars_cmd.lastIndexOf("'");
+  for(int i = state_begin + 1; i < state_end; i++)
+  {
+    cmd.concat(pars_cmd[i]);
+  }
+
+  state_begin = pars_data.indexOf("'", pars_data.indexOf(':'));
+  state_end = pars_data.lastIndexOf("'");
+  for(int i = state_begin + 1; i < state_end; i++)
+  {
+    data.concat(pars_data[i]);
+  }
+  
+  Serial.println(state);
+  Serial.println(cmd);
+  Serial.println(data);
+    /*
   state_begin = pars.indexOf('state') + 5;
   state_end = pars.indexOf(',', pars.indexOf('state')) - 2;
   for(int i = state_begin; i <= state_end; i++)
@@ -20,7 +50,7 @@ void loop()
     Serial.print(pars[i]);
   }
   Serial.print('\n');
-  /*
+  
   while(pars[j] != ',')
   {
     
--- raspi_board_machine/sys_command/python.py
+++ raspi_board_machine/sys_command/python.py
@@ -6,11 +6,17 @@ Created on Tue Aug 31 17:35:40 2021
 @author: honepa
 """
 
+import json
+from serial import Serial, SerialException
+
 arduino_in = {
         "state" : "ready",
         "cmd"   : "ready",
-        "data"  : 0
+        "data"  : '0'
         }
 pars = dict()
 pars = {'state': 'ready', 'cmd': 'ready', 'data': 0}
-print(pars.get('state'))
+k = json.dumps(arduino_in, indent = 0)
+arduino = Serial(port = '/dev/ttyUSB0', baudrate = 9600, timeout = 2)
+arduino.write(k.encode())
+print(k)
