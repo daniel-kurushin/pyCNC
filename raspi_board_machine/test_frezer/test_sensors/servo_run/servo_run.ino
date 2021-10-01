691770c0a6799b07f2a96dd49fc1b5fd217abb08
honepa <cot1884@gmail.com>
2021-10-01 16:06:55 +0500

    servo

---
+++ raspi_board_machine/test_frezer/test_sensors/servo_run/servo_run.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+
+Servo lazer_servo;
+
+void setup()
+{
+  lazer_servo.attach(11);
+}
+
+void loop()
+{
+  lazer_servo.write(0);
+  delay(2000);
+  lazer_servo.write(180);
+  delay(2000);
+}
