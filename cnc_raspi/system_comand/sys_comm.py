739f2d19929f2609519ad339b5125acb709e4b22
honepa <cot1884@gmail.com>
2023-01-28 22:18:56 +0500

    good read

--- cnc_raspi/system_comand/sys_comm.py
+++ cnc_raspi/system_comand/sys_comm.py
@@ -13,19 +13,18 @@ def arduino_send(data):
         ser.write(i.to_bytes(2, byteorder='big',signed=1))
     ser.write(END.to_bytes(2, byteorder='big',signed=0))
 
-def arduino_read():
-    arduino_status  = str(ser.read_until('<').decode('ascii'))
+def arduino_read(old_status):
+	try:
+		arduino_status  = str(ser.read_until('<').decode('ascii'))
+		if len(arduino_status) > 1:
+			indices = [i for i in range(len(''.join(arduino_status.split('\r')).split('\n'))) if ''.join(arduino_status.split('\r')).split('\n')[i] == '<']
+			arduino_status = [float(''.join(arduino_status.split('\r')).split('\n')[i]) for i in range(indices[-2] + 1, indices[-1])]
+    		return arduino_status
+    	else:
+    		return old_status
+    except:
+    	return old_status
     #print(arduino_status)
-    if len(arduino_status) < 0:
-    	indices = [i for i in range(len(''.join(arduino_status.split('\r')).split('\n'))) if ''.join(arduino_status.split('\r')).split('\n')[i] == '<']
-    	arduino_status = [int(''.join(arduino_status.split('\r')).split('\n')[i]) for i in range(indices[-2] + 1, indices[-1])]
-    	if arduino_status[-3] != 1:
-    		print("endstop X")
-    	if arduino_status[-2] != 1:
-    		print("endstop Y")
-    	if arduino_status[-1] != 1:
-    		print("endstop Z")
-    return arduino_status
 
 
 if __name__ == "__main__":
@@ -35,4 +34,23 @@ if __name__ == "__main__":
     arduino_send(data)
     sleep(15)
     arduino_read()
+    status = [0, 0, 666, 0, 0, 0, 0]
+    for i in range(100):
+		data = [50, 0, 0, 0]
+		arduino_send(data)
+		status1 = ser.readlines()
+		status = read(status)
+		while(not ((status[3] >= 4999000) and (status[3] <= 5000999))):
+				  sleep(0.1)
+				  status = read(status)
+		data = [-50, 0, 0, 0]
+		arduino_send(data)
+		status1 = ser.readlines()
+		sleep(0.4)
+		status = read(status)
+		while(not ((status[3] >= -10) and (status[3] <= 1000))):
+			sleep(0.1)
+     		status = read(status)
+     	if i % 10 == 0:
+			print(i)
     ser.close()
--- cnc_raspi/system_comand/sys_command/sys_command.ino
+++ cnc_raspi/system_comand/sys_command/sys_command.ino
@@ -26,9 +26,9 @@
 #define ar analogRead
 #define aw analogWrite
 
-#define X_STEPS_MM 796.0
-#define Y_STEPS_MM 796.0
-#define Z_STEPS_MM 796.0
+#define X_STEPS_MM 800
+#define Y_STEPS_MM 800
+#define Z_STEPS_MM 800
 
 #define INIT 0
 #define FSB  1
@@ -36,7 +36,14 @@
 #define ANG  4
 #define END  5
 
-float x_now, y_now, z_now;
+int x;
+int data;
+int state = INIT;
+int n, end_x, end_y, end_z;
+int L;
+float y_now, z_now;
+float zero_freza = 10.56;
+
 void setup_x()
 {
   pm(xEN, 1); dw(xEN, 1);
@@ -80,46 +87,48 @@ void z_enable(int a)
   dw(zEN, !a);
 }
 
-void x_step(int dir, float speed_x)
+long x_step(int dir, float speed_x)
 {
   bool d = dir == FRW ? 1 : 0;
   dw(xST, 0); dw(xDR, d);
-  delayMicroseconds(round(round((pow(speed_x * X_STEPS_MM, -1) * pow(10, 5)))/2));
+  delayMicroseconds(round(round((pow(speed_x * X_STEPS_MM, -1) * pow(10, 5))) / 2));
   dw(xST, 1); dw(xDR, !d);
-  delayMicroseconds(round(round((pow(speed_x * X_STEPS_MM, -1) * pow(10, 5)))/2));
-  x_now += dir * (1 / X_STEPS_MM);
+  delayMicroseconds(round(round((pow(speed_x * X_STEPS_MM, -1) * pow(10, 5))) / 2));
+  return dir * 125; // round((1 / X_STEPS_MM) * 100000)
 }
 
 void y_step(int dir, float speed_y)
 {
   bool d = dir == FRW ? 1 : 0;
   dw(yST, 0); dw(yDR, d);
-  delayMicroseconds(round(round((pow(speed_y * Y_STEPS_MM, -1) * pow(10, 5)))/2));
+  delayMicroseconds(round(round((pow(speed_y * Y_STEPS_MM, -1) * pow(10, 5))) / 2));
   dw(yST, 1); dw(yDR, !d);
-  delayMicroseconds(round(round((pow(speed_y * Y_STEPS_MM, -1) * pow(10, 5)))/2));
-  y_now += dir * (1 / Y_STEPS_MM);
+  delayMicroseconds(round(round((pow(speed_y * Y_STEPS_MM, -1) * pow(10, 5))) / 2));
+  y_now += dir * 125;
 }
 
 void z_step(int dir, float speed_z)
 {
   bool d = dir == FRW ? 1 : 0;
   dw(zST, 0); dw(zDR, d);
-  delayMicroseconds(round(round((pow(speed_z * Z_STEPS_MM, -1) * pow(10, 5)))/2));
+  delayMicroseconds(round(round((pow(speed_z * Z_STEPS_MM, -1) * pow(10, 5))) / 2));
   dw(zST, 1); dw(zDR, !d);
-  delayMicroseconds(round(round((pow(speed_z * Z_STEPS_MM, -1) * pow(10, 5)))/2));
-  z_now += dir * (1 / Z_STEPS_MM);
+  delayMicroseconds(round(round((pow(speed_z * Z_STEPS_MM, -1) * pow(10, 5))) / 2));
+  z_now += dir * 0.00125;
 }
 
-void x_go(float mm, float speed_x)
+long x_go(long x,  float mm, float speed_x)
 {
+  long x1 = x;
   float steps = mm * X_STEPS_MM;
   x_enable(1);
   int d = steps > 0 ? FRW : BCK;
   for (long i = 0; i < abs(steps); i++)
   {
-    x_step(d, speed_x); 
+    x1 += x_step(d, speed_x);
   }
   x_enable(0);
+  return x1;
 }
 
 void y_go(float mm, float speed_y)
@@ -129,7 +138,7 @@ void y_go(float mm, float speed_y)
   int d = steps > 0 ? FRW : BCK;
   for (long i = 0; i < abs(steps); i++)
   {
-    y_step(d, speed_y); 
+    y_step(d, speed_y);
   }
   y_enable(0);
 }
@@ -149,25 +158,25 @@ void z_go(float mm, float speed_z)
 float get_zero_freza()
 {
   float freza_mm;
-  while(dr(F_END) and (z_now < 80))
+  while (dr(F_END) and (z_now < 80))
   {
     z_go(0.01, 0.25);
   }
   freza_mm = z_now;
   int count = 0;
-  while(dr(Z_END) and (count < 80))
+  while (dr(Z_END) and (count < 80))
   {
     z_go(-1, 1);
     count++;
   }
   count = 0;
   z_go(5, 1);
-  while(dr(Z_END) and (count < 80))
+  while (dr(Z_END) and (count < 80))
   {
     z_go(-0.25, 0.05);
     count++;
   }
-  while(!dr(Z_END))
+  while (!dr(Z_END))
   {
     z_go(0.01, 0.05);
   }
@@ -178,60 +187,60 @@ float get_zero_freza()
 void init_ramps()
 {
   int count = 0;
-  while(dr(X_END) and (count < 270))
+  long x;
+  while (dr(X_END) and (count < 270))
   {
-    x_go(-1, 1);
+    x_go(x, -1, 1);
     count++;
   }
   count = 0;
-  x_go(3, 1);
-  while(dr(X_END) and (count < 270))
+  x_go(x, 3, 1);
+  while (dr(X_END) and (count < 270))
   {
-    x_go(-0.01, 0.05);
+    x_go(x, -0.01, 0.05);
     count++;
   }
-  while(!dr(X_END))
+  while (!dr(X_END))
   {
-    x_go(0.01, 0.05);
+    x_go(x, 0.01, 0.05);
   }
 
   count = 0;
-  while(dr(Y_END) and (count < 270))
+  while (dr(Y_END) and (count < 270))
   {
     y_go(-1, 1);
     count++;
   }
   count = 0;
   y_go(3, 1);
-  while(dr(Y_END) and (count < 270))
+  while (dr(Y_END) and (count < 900))
   {
     y_go(-0.01, 0.05);
     count++;
   }
-  while(!dr(Y_END))
+  while (!dr(Y_END))
   {
     y_go(0.01, 0.05);
   }
 
-  
+
   count = 0;
-  while(dr(Z_END) and (count < 80))
+  while (dr(Z_END) and (count < 80))
   {
     z_go(-1, 1);
     count++;
   }
   count = 0;
   z_go(5, 1);
-  while(dr(Z_END) and (count < 80))
+  while (dr(Z_END) and (count < 80))
   {
     z_go(-0.25, 0.05);
     count++;
   }
-  while(!dr(Z_END))
+  while (!dr(Z_END))
   {
     z_go(0.01, 0.05);
   }
-  x_now = 0;
   y_now = 0;
   z_now = 0;
 }
@@ -248,77 +257,79 @@ void setup()
   init_ramps();
 }
 
-int x;
-int data;
-int state = INIT;
-int n, end_x, end_y, end_z;
-int L;
-
 void loop()
 {
-  float zero_freza = 10.56;
-  if (Serial.available()) {
-    data = Serial.read() << 8 | Serial.read();
-    switch (state) {
-      case INIT:
-        L = 0;
-        n = 0;
-        if (data = 255) {
-          state = FSB;
-        }
-        break;
-      case FSB:
-        if (data = 255) {
-          state = SSB;
-        }
-        else {
-          state = INIT;
-        }
-        break;
-      case SSB:
-        if (data < 20) {
-          L = data;
-          state = ANG;
-          n = 0;
-        }
-        else {
-          state = INIT;
-        }
-        break;
-      case ANG:
-        if (data != 253)
-        {
-          switch (n++) {
-            case 0:
-              x_go(data / 100, 1);
-              break;
-            case 1:
-              y_go(data / 100, 1);
-              break;
-            case 2:
-              z_go(data / 100, 1);
-              break;
-            case 3:
-              analogWrite(FREZA, data);
-              break;
-          }
-        }
-        else
-        {
-          state = INIT;
-          n = 0;
+  long x_now = 0;
+  float d = 0;
+  //x_now = x_go(x_now, 10, 1);
+  while (1)
+  {
+    if (Serial.available()) {
+      data = Serial.read() << 8 | Serial.read();
+      switch (state) {
+        case INIT:
           L = 0;
-        }
-        break;
-    }
+          n = 0;
+          if (data = 255) {
+            state = FSB;
+          }
+          break;
+        case FSB:
+          if (data = 255) {
+            state = SSB;
+          }
+          else {
+            state = INIT;
+          }
+          break;
+        case SSB:
+          if (data < 20) {
+            L = data;
+            state = ANG;
+            n = 0;
+          }
+          else {
+            state = INIT;
+          }
+          break;
+        case ANG:
+          if (data != 253)
+          {
+            switch (n++) {
+              case 0:
+                d = data / 100;
+                x_now = x_go(x_now, d, 1);
+                break;
+              case 1:
+                d = data / 100;
+                y_go(d, 1);
+                break;
+              case 2:
+                d = data / 100;
+                z_go(d, 1);
+                break;
+              case 3:
+                analogWrite(FREZA, data);
+                break;
+            }
+          }
+          else
+          {
+            state = INIT;
+            n = 0;
+            L = 0;
+          }
+          break;
+      }
 
-  }
+    }
     Serial.println(state);
     Serial.println(data);
     Serial.println(666);
-    Serial.println(x);
+    Serial.println(x_now);
     Serial.println(y_now);
     Serial.println(z_now);
     Serial.println(zero_freza);
     Serial.println("<");
+  }
 }
