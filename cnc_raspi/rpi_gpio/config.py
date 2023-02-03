26025dd2083652b02eb6f2ae517c5a19a574d5f4
honepa <cot1884@gmail.com>
2023-02-03 22:10:33 +0500

    feniks

---
+++ cnc_raspi/rpi_gpio/config.py
@@ -0,0 +1,22 @@
+X_END = 22
+Y_END = 24
+Z_END = 18
+F_END = 16
+
+x_St = 3
+x_Dr = 5
+x_En = 7
+
+y_St = 11
+y_Dr = 13
+y_En = 15
+
+z_St = 8
+z_Dr = 10
+z_En = 12
+
+Freza = 35
+
+X_STEPS_MM = 8
+Y_STEPS_MM = 8
+Z_STEPS_MM = 8
