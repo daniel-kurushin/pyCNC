634536dd45eb0b31e7e6f643c8cb6b73c0901a6d
honepa <cot1884@gmail.com>
2023-05-09 20:16:07 +0500

    photo

--- cnc_raspi/photo_test/panorama_test.py
+++ cnc_raspi/photo_test/panorama_test.py
@@ -1,8 +1,18 @@
 import cv2
-image_paths=['/tmp/1.jpg','/tmp/2.jpg','/tmp/3.jpg']
+from glob import glob
+image_paths=[]
 # initialized a list of images
+for i in range(26):
+	image_paths.append(f'/tmp/cnc_full/[{1000000 * i}, 3200000, 0, 0].jpeg')	
+#image_paths.sort()
+image_paths.append('/tmp/cnc_full/[25500000, 3200000, 0, 0].jpeg')
+for i in range(26):
+	image_paths.append(f'/tmp/cnc_full/[{1000000 * i}, 4200000, 0, 0].jpeg')
+for i in range(26):
+	image_paths.append(f'/tmp/cnc_full/[{1000000 * i}, 5200000, 0, 0].jpeg')
 imgs = []
-  
+#image_paths.append('/tmp/travel/IMG_20230204_125248.jpg')
+#image_paths.append('/tmp/travel/IMG_20230204_124709.jpg')
 for i in range(len(image_paths)):
     imgs.append(cv2.imread(image_paths[i]))
     imgs[i]=cv2.resize(imgs[i],(0,0),fx=0.4,fy=0.4)
@@ -24,6 +34,6 @@ else:
     print('Your Panorama is ready!!!')
   
 # final output
-cv2.imshow('final result',output)
-  
-cv2.waitKey(0)
+#cv2.imshow('final result',output)
+cv2.imwrite('out_cnc_full.jpg', output)
+#cv2.waitKey(0)
